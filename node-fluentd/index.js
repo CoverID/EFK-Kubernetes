3160e0e8d949e29d4263031057e603a37cb2c092
Lutfi Maulana <lutfi.maulana@ecomindo.com>
2022-02-23 09:23:14 +0700

    update app

--- node-fluentd/index.js
+++ node-fluentd/index.js
@@ -1,59 +1,52 @@
-const express = require('express');
-const FluentClient = require('@fluent-org/logger').FluentClient;
+const express = require("express");
+const FluentClient = require("@fluent-org/logger").FluentClient;
 const app = express();
 
-const logger = new FluentClient('fluentd.test', {
+const logger = new FluentClient("node-app", {
   socket: {
-    host: 'fluentd-fluentd-elasticsearch-forward',
+    host: "fluentd-fluentd-elasticsearch-forward",
     port: 24224,
-    timeout: 3000, // 3 seconds
-  }
+    timeout: 3000,
+  },
 });
 
-// var logger = require('express-fluent-logger');
-
-// app.use(logger('node-app', { host: 'fluentd-fluentd-elasticsearch-forward', port: 24224, timeout: 3.0, responseHeaders: ['x-userid'] }));
-
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   let obj = {
-    endpoints: [
-      "/ping",
-      "/current-date",
-      "/fibo/:n",
-    ]
+    endpoints: ["/ping", "/current-date", "/fibo/:n"],
   };
+  logger.emit("home", obj);
   res.send(obj);
 });
 
-app.get('/ping', function (req, res) {
+app.get("/ping", function (req, res) {
+  logger.emit("/ping");
   res.send("pong");
 });
 
-
-app.get('/current-date', function (req, res) {
+app.get("/current-date", function (req, res) {
   let obj = {
     name: "current",
-    value: new Date()
+    value: new Date(),
   };
+  logger.emit("/current-date", obj);
   res.send(obj);
 });
 
-app.get('/fibo/:n', function (req, res) {
+app.get("/fibo/:n", function (req, res) {
   let obj = {
     name: "fibo",
-    value: fibo(req.params.n)
+    value: fibo(req.params.n),
   };
+  logger.emit("/fibo", obj);
   res.send(obj);
 });
 
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
 
-
-function fibo(n) { // 1
-  if (n < 2)
-    return 1;
-  else   return fibo(n - 2) + fibo(n - 1);
+function fibo(n) {
+  // 1
+  if (n < 2) return 1;
+  else return fibo(n - 2) + fibo(n - 1);
 }
